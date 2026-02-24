886bda7fe45b2279cb6081f860bcc671510dbeba
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-24 10:23:10 -0700

    create 1 file and update 1 file

--- src/pages/home/components/slider/BannerSlider.jsx
+++ src/pages/home/components/slider/BannerSlider.jsx
@@ -1,21 +1,37 @@
-import BannerCard from './BannerCard';
-import banner1 from '../../../../assets/img/01.png';
-import banner2 from '../../../../assets/img/02.png';
-import banner3 from '../../../../assets/img/03.png';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+import BannerCard from "./BannerCard";
+import banner1 from "../../../../assets/img/01.png";
+import banner2 from "../../../../assets/img/02.png";
+import banner3 from "../../../../assets/img/03.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 const slidersInfo = [
-  { title: 'امروز سبزیجات سالم و ارگانیک خود را آنلاین دریافت کنید.', img: banner1, color: 'bg-yellow-100' },
-  { title: 'ما تازه ترین محصولات را برای شما ارسال می کینم.', img: banner2, color: 'bg-blue-100' },
-  { title: 'ما مسیر مزرعه تا خانه شمارا هموار کردیم.', img: banner3, color: 'bg-emerald-100' },
+  {
+    title: "امروز سبزیجات سالم و ارگانیک خود را آنلاین دریافت کنید.",
+    img: banner1,
+    color: "bg-yellow-100",
+  },
+  {
+    title: "ما تازه ترین محصولات را برای شما ارسال می کینم.",
+    img: banner2,
+    color: "bg-blue-100",
+  },
+  {
+    title: "ما مسیر مزرعه تا خانه شمارا هموار کردیم.",
+    img: banner3,
+    color: "bg-emerald-100",
+  },
 ];
 
 export default function BannerSlider() {
   return (
-    <Swiper pagination={true} modules={[Pagination]} className="w-full h-full col-span-12 md:col-span-8">
+    <Swiper
+      pagination={true}
+      modules={[Pagination]}
+      className="w-full h-full col-span-12 md:col-span-8"
+    >
       {slidersInfo.map((item, index) => {
         return (
           <SwiperSlide key={index} className="">
