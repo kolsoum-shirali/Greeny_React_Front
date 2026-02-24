c0ebed6f8683fd0e00f6d37ae2270f87d1dd9fb2
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-24 12:50:05 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import HomePage from './pages/home/index'
+import Footer from './components/layout/Footer';
 function App() {
   return (
     <div className="App bg-Slate-100">
       <HomePage />
+      <Footer/>
     </div>
   );
 }
