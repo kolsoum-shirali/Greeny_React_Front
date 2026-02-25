80e7189908adb764e6ec9cc72e66d0863e93867e
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-25 02:18:34 -0700

    feat: create BlogCard.jsx

---
+++ src/pages/home/components/blogs/BlogCard.jsx
@@ -0,0 +1,22 @@
+export default function BlogCard({ blog }) {
+  return (
+    <div className="shadow-sm p-4 border border-gray-400/20">
+      <img src={blog.img} alt="" className="w-full h-[300px] pb-2" />
+      <div className="space-y-3 mt-3">
+        <div className="flex gap-2">
+          <p className="text-gray-500 text-sm">
+            <i className="icon-user-o text-green-800"></i>ادمین
+          </p>
+          <p className="text-gray-500 text-sm">
+            <i className=" icon-calendar text-green-800"></i>10 بهمن 1404
+          </p>
+        </div>
+        <h3 className="text-lg line-clamp-2 font-bold">{blog.title}</h3>
+        <p className="text-base line-clamp-3 text-gray-500">{blog.desc}</p>
+        <p className="text-green-800 font-semibold">
+          بیشتر بخوانید <i className=" icon-left-big"></i>
+        </p>
+      </div>
+    </div>
+  );
+}
