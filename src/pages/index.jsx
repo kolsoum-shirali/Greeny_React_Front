7dccef2a2d7d3b69cef8a29d0cdc7301939f7353
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-25 09:02:53 -0700

    feat: create src/pages/index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+//Better way of importing
+
+export { default as HomePage } from "./home";
+export { default as Login } from "./login";
+export { default as Register } from "./register";
+export { default as NotFound } from "./notFound";
+
+
+/* -- Layout -- */
+export { default as Footer } from "../components/layout/Footer";
+export { default as Header } from "../components/layout/Header";
