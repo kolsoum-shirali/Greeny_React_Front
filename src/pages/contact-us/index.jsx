da38cd5f4eedeeeb035b659a5f2c35d9f9a9117d
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-26 10:44:45 -0700

    update src/pages/contact-us/index.jsx

--- src/pages/contact-us/index.jsx
+++ src/pages/contact-us/index.jsx
@@ -1,7 +1,17 @@
-export default function ContactUs(){
-  return(
+import BreadCrumbsBanner from "../../components/common/BreadCrumbsBanner";
+import ContactWays from "./components/ContactWays";
+const options = [
+  { title: "خانه", link: "/" },
+  { title: "ارتباط با ما", link: "" },
+];
+
+export default function ContactUs() {
+  return (
     <div>
-      
+      <BreadCrumbsBanner options={options} caption="ارتباط با ما" />
+      <div className="container mx-auto p-5 mt-7 md:mt-16 space-y-10 lg:space-y-32">
+        <ContactWays />
+      </div>
     </div>
-  )
+  );
 }
