9a6f615a1a95a29e3c4c57e889bad45115e9c98a
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-26 03:48:30 -0700

    update src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,8 @@
 export { default as HomePage } from "./home";
 export { default as Login } from "./login";
 export { default as Register } from "./register";
+export { default as AboutUs } from "./about-us";
+export { default as ContactUs } from "./contact-us";
 export { default as NotFound } from "./notFound";
 
 
