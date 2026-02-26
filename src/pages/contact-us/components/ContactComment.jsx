a9832027cc127fd3be6a6c07847200ae38888ac8
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-26 11:14:51 -0700

    feat: create ContactComment.jsx

---
+++ src/pages/contact-us/components/ContactComment.jsx
@@ -0,0 +1,16 @@
+import ContactForm from "./ContactForm";
+import Map from "../../../assets/img/map.png";
+export default function ContactComment() {
+  return (
+    <div className="grid grid-cols-2 gap-5 lg:gap-10">
+      <div className="col-span-2 lg:col-span-1">
+        <div className="flex justify-center">
+          <img src={Map} alt="" className="w-full h-auto" />
+        </div>
+      </div>
+      <div className="col-span-2 lg:col-span-1">
+        <ContactForm />
+      </div>
+    </div>
+  );
+}
