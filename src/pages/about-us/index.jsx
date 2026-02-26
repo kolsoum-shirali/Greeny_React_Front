402cf399e9be3637311a273561f19a8a19b1726c
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-26 10:17:27 -0700

    delete 2 files, create 1 file and update 1 file

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -1,11 +1,15 @@
 import AboutCompany from "./components/AboutCompany";
 import WhyChoose from "./components/WhyChoose";
-import AboutBanner from "./components/AboutBanner";
+import BreadCrumbsBanner from "../../components/common/BreadCrumbsBanner";
+const options = [
+  { title: "خانه", link: "/" },
+  { title: "درباره ما", link: "" },
+];
 
 export default function AboutUs() {
   return (
     <div>
-      <AboutBanner />
+      <BreadCrumbsBanner options={options} caption="درباره شرکت ما" />
       <div className="container mx-auto p-5 mt-7 md:mt-16 space-y-10 lg:space-y-32">
         <AboutCompany />
         <WhyChoose />
