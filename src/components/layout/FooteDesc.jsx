5a92e85c8eefd05613827601f361723f7c0c7470
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-02-27 02:41:40 -0700

    update FooteDesc.jsx

--- src/components/layout/FooteDesc.jsx
+++ src/components/layout/FooteDesc.jsx
@@ -1,10 +1,5 @@
 import Logo from "../../assets/img/logo.png";
-const socials = [
-  { icon: "icon-whatsapp" },
-  { icon: "icon-telegram" },
-  { icon: "icon-instagram" },
-  { icon: "icon-linkedin" },
-];
+import Socials from "../common/Socials";
 export default function FooteDesc() {
   return (
     <div className="col-span-12 md:col-span-4 lg:col-span-6 space-y-8 ">
@@ -14,17 +9,7 @@ export default function FooteDesc() {
         از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
         سطرآنچنان که لازم است.
       </p>
-      <ul className="flex gap-2">
-        {socials.map((social, index) => {
-          return (
-            <li key={index}>
-              <i
-                className={`${social.icon} text-lg lg:text-xl bg-green-600 rounded-full aspect-square p-2 text-white cursor-pointer`}
-              ></i>
-            </li>
-          );
-        })}
-      </ul>
+      <Socials />
     </div>
   );
 }
