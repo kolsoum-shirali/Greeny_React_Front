467b5edf57950fec259b6a6940189edf76d8b53e
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-14 12:49:48 -0600

    feat: create blogs.api.js

---
+++ src/api/blogs.api.js
@@ -0,0 +1,16 @@
+import { baseUrl, defaultHeaders } from "./index.api";
+
+export const fetchBlogs = async () => {
+  const response = await fetch(`${baseUrl}/blogs`, {
+    method: "GET",
+    headers: defaultHeaders,
+  });
+  if (!response.ok) {
+    const errorText = await response.statusText;
+    throw new Error(
+      `HTTP error! status: ${response.status}, message: ${errorText}`,
+    );
+  }
+  const { data } = await response.json();
+  return data;
+};
