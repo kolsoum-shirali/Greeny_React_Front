6d3a2866a864a6dd3fbf88f5d52fe4c7cbe104d5
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-14 08:11:48 -0600

    update FooteDesc.jsx

--- src/components/layout/FooteDesc.jsx
+++ src/components/layout/FooteDesc.jsx
@@ -3,7 +3,7 @@ import Socials from "../common/Socials";
 export default function FooteDesc() {
   return (
     <div className="col-span-12 md:col-span-4 lg:col-span-6 space-y-8 ">
-      <img src={Logo} className="w-44" />
+      <img src={Logo} alt="logo-footer" className="w-44" />
       <p className="text-sm lg:text-base">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
         از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
