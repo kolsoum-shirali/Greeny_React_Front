8f342db450f0cceabb8aff5a45224844b04d589d
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-14 08:13:51 -0600

    update SingleComment.jsx

--- src/pages/home/components/comments/SingleComment.jsx
+++ src/pages/home/components/comments/SingleComment.jsx
@@ -1,6 +1,12 @@
 import Rating from "../../../../components/common/Rating";
+import { useState } from "react";
 import gril from "../../../../assets/img/gril.jpeg";
 export default function SingleComment({ comment }) {
+  const [productRate, setProductRate] = useState();
+  const changeProductRate = (newValue) => {
+    setProductRate(newValue);
+  };
+
   return (
     <div className="relative bg-gray-100 px-10 py-5 rounded-md space-y-3">
       <p className="text-justify line-clamp-6 text-gray-700 text-base">
@@ -8,7 +14,11 @@ export default function SingleComment({ comment }) {
       </p>
       <div className="flex flex-col justify-center items-center">
         <h3 className="text-sm">{comment.author}</h3>
-        <Rating />
+        <Rating
+          rateVal={productRate}
+          isReadable={false}
+          onRatingChange={changeProductRate}
+        />
         <div className="w-20 h-20 aspect-square rounded-full overflow-hidden">
           <img src={gril} alt="" />
         </div>
