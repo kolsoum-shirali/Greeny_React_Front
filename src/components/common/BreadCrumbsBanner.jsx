bd45cac6d8a0dd20f981e01677d7cd14d1108243
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-15 06:26:12 -0600

    update BreadCrumbsBanner.jsx

--- src/components/common/BreadCrumbsBanner.jsx
+++ src/components/common/BreadCrumbsBanner.jsx
@@ -1,25 +1,40 @@
 import Banner from "../../assets/img/single-banner.jpg";
-export default function BreadCrumbsBanner({options,caption}) {
+import { Link } from "react-router-dom";
+
+export default function BreadCrumbsBanner({ options, caption }) {
   return (
-    <div className="relative h-60 flex justify-center">
-      <img src={Banner} alt="" className="w-full h-auto object-cover" />
-      <div className="absolute z-1 inset-0 bg-green-500/50 flex flex-col text-white space-y-4 justify-center items-center">
-        <h1 className="font-semibold text-3xl">{caption}</h1>
-        <ul className="flex">
-          {options.map((item, index) => (
-            <li
-              key={index}
-              className={`text-lg ${item.link ? "" : "text-black/50"}`}
-            >
-              {item.title}
-              {options.length - 1 === index ? (
-                ""
-              ) : (
-                <i className="icon-left-open text-sm"></i>
-              )}
-            </li>
-          ))}
-        </ul>
+    <div className="relative flex h-60 justify-center overflow-hidden">
+      <img src={Banner} alt={Banner} className="h-full w-full object-cover" />
+      <div className="absolute inset-0 z-1 flex items-center justify-center bg-green-500/50 text-center text-white">
+        <div className="w-11/12 px-4">
+          <h1 className="mb-4 text-lg font-semibold md:text-3xl break-words">
+            {caption}
+          </h1>
+          <ul className="flex flex-wrap justify-center gap-2">
+            {options.map((item, index) => (
+              <li
+                key={index}
+                className={`max-w-full text-sm md:text-lg break-words ${
+                  !item.link && "text-black"
+                }`}
+              >
+                {item.link ? (
+                  <Link
+                    to={item.link}
+                    className="flex items-center gap-1 break-words"
+                  >
+                    <span className="break-words">{item.title}</span>
+                    {options.length - 1 !== index && (
+                      <i className="icon-left-open text-sm"></i>
+                    )}
+                  </Link>
+                ) : (
+                  <span className="break-words">{item.title}</span>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
