1a2d04f87dedc9eba34b028ee1135e36ce986540
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-15 06:30:00 -0600

    update BlogDetail.jsx

--- src/pages/blog/BlogDetail.jsx
+++ src/pages/blog/BlogDetail.jsx
@@ -1,57 +1,104 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import BreadCrumbsBanner from "../../components/common/BreadCrumbsBanner";
 import ContactForm from "../../components/common/ContactForm";
-import PreviewPicture from "../../assets/img/4.png";
 import PopularBlogs from "./components/PopularBlogs";
 import FollowUs from "./components/FollowUs";
 import BlogComments from "./components/BlogComments";
-const options = [
-  { title: "خانه", link: "/" },
-  { title: "وبلاگ", link: "/blog" },
-  { title: "جزئیات مطلب", link: "/blog" },
-];
-
+import { fetchSingleBlog } from "../../api/blogs.api";
+import styles from "../../styles/css/blog.module.css";
+import BlogDetailLoading from "./components/BlogDetailLoading";
+import CommentLoading from "../../components/common/CommentLoading";
 export default function BlogDetail() {
+  const { id: blogId } = useParams();
+  const [blog, setBlog] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const options = [
+    { title: "خانه", link: "/" },
+    { title: "وبلاگ", link: "/blog" },
+    { title: blog.title || "", link: "" },
+  ];
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const loadBlog = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await fetchSingleBlog(blogId);
+
+        if (isMounted) {
+          setBlog(data);
+          console.log(data, "fd");
+        }
+      } catch (e) {
+        console.error("Error fetching blogs:", e.message);
+        if (isMounted) {
+          setError(e.message);
+        }
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    loadBlog();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
   return (
     <div>
-      <BreadCrumbsBanner options={options} caption="جزئیات مطلب" />
+      <BreadCrumbsBanner options={options} caption={blog.title} />
       <div className="container mx-auto p-5 mt-7 md:mt-16 space-y-10 lg:space-y-32">
         <div className="grid grid-cols-12 md:gap-10">
           <div className="col-span-12 lg:col-span-8 space-y-5 md:space-y-10">
-            <div>
-              <img src={PreviewPicture} alt="" className="mx-auto" />
-              <div className="mt-10">
-                <h1 className="font-semibold text-xl text-center">
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                  استفاده از طراحان گرافیک است.
-                </h1>
-                <p className="text-gray-500 leading-7 mt-5 text-justify">
-                  مداد رنگی ها مشغول بودند به جز مداد سفید، هیچکس به او کار
-                  نمیداد، همه میگفتند : تو به هیچ دردی نمیخوری، یک شب که مداد
-                  رنگی ها تو سیاهی شب گم شده بودند، مداد سفید تا صبح ماه کشید
-                  مهتاب کشید و انقدر ستاره کشید که کوچک و کوچکتر شد صبح توی جعبه
-                  مداد رنگی جای خالی او با هیچ رنگی پر نشد، به یاد هم باشیم شاید
-                  فردا ما هم در کنار هم نباشیم. لورم ایپسوم متن ساختگی با تولید
-                  سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
-                  چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
-                  است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع
-                  با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و
-                  سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
-                  طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای
-                  علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد
-                  کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در
-                  ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز
-                  شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل
-                  دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-                </p>
+            {loading && !error && (
+              <div class="rounded-md border p-4">
+                <div class="animate-pulse space-y-6">
+                  <BlogDetailLoading />
+                  <div className="space-y-8">
+                    {[...Array(3)].map((_, index) => (
+                      <div key={index}>
+                        <CommentLoading />
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <BlogComments />
-            </div>
-            <div className="shadow-md rounded-md p-5 md:p-10 border border-gray-400/20">
-              <h3 className="text-lg font-semibold mb-5">کامنت بگذارید</h3>
-              <ContactForm />
-            </div>
+            )}
+            {!loading && !error && blog && (
+              <div className="space-y-10">
+                <div className="space-y-5 md:space-y-12">
+                  <div className="h-72 md:h-[500px] overflow-hidden flex justify-center rounded-md">
+                    <img
+                      src={`${process.env.REACT_APP_BASE_URL_IMG}${blog.img}`}
+                      alt={blog.img}
+                      className="w-full h-auto object-cover rounded-md 
+"
+                    />
+                  </div>
+                  <h1 className="font-semibold text-2xl text-center">
+                    {blog.title}
+                  </h1>
+
+                  <div
+                    className={`${styles["blog-wrap"]} text-gray-500 leading-7 text-justify`}
+                    dangerouslySetInnerHTML={{ __html: blog.desc }}
+                  />
+                </div>
+                <div>
+                  <BlogComments />
+                </div>
+                <div className="shadow-md rounded-md p-5 md:p-10 border border-gray-400/20">
+                  <h3 className="text-lg font-semibold mb-5">کامنت بگذارید</h3>
+                  <ContactForm />
+                </div>
+              </div>
+            )}
           </div>
           <div className="col-span-12 lg:col-span-4 relative">
             <div className="sticky inset-0 space-y-7 ">
