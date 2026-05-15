1e0b1b68fe953cd39f4e5d0684ac94f564482baa
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-15 06:19:34 -0600

    update blogs.api.js

--- src/api/blogs.api.js
+++ src/api/blogs.api.js
@@ -14,3 +14,17 @@ export const fetchBlogs = async () => {
   const { data } = await response.json();
   return data;
 };
+export const fetchSingleBlog = async (blogId) => {
+  const response = await fetch(`${baseUrl}/blogs/${blogId}`, {
+    method: "GET",
+    headers: defaultHeaders,
+  });
+  if (!response.ok) {
+    const errorText = await response.statusText;
+    throw new Error(
+      `HTTP error! status: ${response.status}, message: ${errorText}`,
+    );
+  }
+  const { data } = await response.json();
+  return data;
+};
