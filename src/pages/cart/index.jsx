acc3a2d1a75f6f3221ca44ee23d00245cc158422
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-16 14:56:03 -0600

    feat : add OrdersList in card page

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,3 +1,8 @@
+import OrdersList from "./components/OrdersList";
 export default function CartPage() {
-  return <div>cart</div>;
+  return (
+    <div className="container mx-auto p-5 mt-7 md:mt-16">
+      <OrdersList />
+    </div>
+  );
 }
