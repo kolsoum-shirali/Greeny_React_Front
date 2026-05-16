735c50878f2f67a9f0ba24cade1557b621ddbffe
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-16 04:30:49 -0600

    feat : get data from api

--- src/pages/products/ProductsDetail.jsx
+++ src/pages/products/ProductsDetail.jsx
@@ -1,23 +1,62 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import ContactForm from "../../components/common/ContactForm";
 import Rating from "../../components/common/Rating";
 import BreadCrumbsBanner from "../../components/common/BreadCrumbsBanner";
 import ProductComments from "./components/ProductComments";
 import ProductsInfo from "./components/ProductsInfo";
 import Product from "./components/Product";
-
-const options = [
-  { title: "خانه", link: "/" },
-  { title: "فروشگاه", link: "" },
-];
+import { fetchSingleProduct } from "../../api/products.api";
 
 export default function ProductsDetail() {
+  const { id: productId } = useParams();
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const options = [
+    { title: "خانه", link: "/" },
+    { title: "فروشگاه", link: "/products" },
+    { title: product.title || "", link: "" },
+  ];
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const loadProduct = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await fetchSingleProduct(productId);
+
+        if (isMounted) {
+          setProduct(data);
+        }
+      } catch (e) {
+        console.error("Error fetching product:", e.message);
+        if (isMounted) {
+          setError(e.message);
+        }
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    loadProduct();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
   return (
     <div>
-      <BreadCrumbsBanner options={options} caption="سینگل محصول" />
+      <BreadCrumbsBanner options={options} caption={product.title} />
       <div className="container mx-auto p-5 mt-7 md:mt-16 space-y-10 lg:space-y-16">
-        <Product />
-        <ProductsInfo />
-        <ProductComments />
+        <Product product={product} />
+        <ProductsInfo product={product} />
+        {product.comments && <ProductComments comments={product?.comments} />}
         <div className="shadow-md rounded-md p-5 md:p-10 border border-gray-400/20">
           <h3 className="text-lg font-semibold mb-5">نظر خود را اضافه کنید</h3>
           <div className="flex justify-center mb-5">
