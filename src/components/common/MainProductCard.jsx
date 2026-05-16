f969e4cb2f79f1ee7b2c1a2ee5abedce02a4ada2
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-16 04:20:37 -0600

    feat: create MainProductCard.jsx

---
+++ src/components/common/MainProductCard.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+export default function MainProductCardd({
+  product,
+  showHeading,
+  productBasePath,
+}) {
+  return (
+    <div className="shadow-sm p-5 border border-gray-400/20 rounded-md">
+      <div className="h-72 md:h-96 overflow-hidden flex justify-center pb-2 rounded-md">
+        <img
+          src={`${process.env.REACT_APP_BASE_URL_IMG}${product.img}`}
+          alt={product.img}
+          className="w-full h-auto object-cover rounded-md hover:scale-110 transition-scale duration-500
+"
+        />
+      </div>
+      <div className="space-y-3 mt-3">
+        {showHeading && (
+          <div className="flex gap-2">
+            <p className="text-gray-500 text-sm">
+              <i className="icon-user-o text-green-800"></i>ادمین
+            </p>
+            <p className="text-gray-500 text-sm">
+              <i className=" icon-calendar text-green-800"></i>
+              {product.date}
+            </p>
+            <p className="text-gray-500 text-sm">
+              <i className=" icon-user-o text-green-800"></i>
+              {product.comment} کامنت
+            </p>
+          </div>
+        )}
+
+        <h3 className="text-lg line-clamp-1 font-bold">{product.title}</h3>
+        <p className="text-base line-clamp-3 text-gray-500 text-justify">
+          {product.shortDesc}
+        </p>
+        <div className="flex">
+          <Link to={`/${productBasePath}/${product.id}`} className="mr-auto">
+            <p className="text-green-800 font-semibold hover:bg-green-800 hover:text-white transition-colors duration-100 delay-75 p-3  rounded-md">
+              بیشتر بخوانید <i className=" icon-left-big"></i>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
