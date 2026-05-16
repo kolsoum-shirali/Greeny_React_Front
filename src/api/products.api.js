d974d087564a900028218996f9a11f12ee260aed
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-16 04:20:05 -0600

    feat : add fetchSingleProduct api

--- src/api/products.api.js
+++ src/api/products.api.js
@@ -14,3 +14,17 @@ export const fetchProducts = async () => {
   const { data } = await response.json();
   return data;
 };
+export const fetchSingleProduct = async (productId) => {
+  const response = await fetch(`${baseUrl}/products/${productId}`, {
+    method: "GET",
+    headers: defaultHeaders,
+  });
+  if (!response.ok) {
+    const errorText = await response.statusText;
+    throw new Error(
+      `HTTP error! status: ${response.status}, message: ${errorText}`,
+    );
+  }
+  const { data } = await response.json();
+  return data;
+};
