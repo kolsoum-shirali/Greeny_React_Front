e7bda7b39460b02e45b3970ffaf383a91423b211
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-16 04:22:05 -0600

    update src/pages/register/index.jsx

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,41 @@
 import Logo from "../../assets/img/logo.png";
 import Divider from "@mui/material/Divider";
+import  { useState } from "react";
 
-export default function Register() {
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:5000/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, name, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setMessage("Registration successful!");
+      console.log(data);
+      console.log("1");
+    } else {
+      console.log("2");
+      const errorData = await response.json();
+      setMessage(
+        `Registration failed: ${errorData.message || response.statusText}`,
+      );
+      console.error("Registration failed:", errorData);
+    }
+  };
+
   return (
     <div className="p-3 lg:p-10">
       <div className="flex flex-col justify-center items-center">
@@ -17,45 +51,63 @@ export default function Register() {
               ثبت نام شما تنها چند دقیقه زمان می برد.
             </h2>
           </div>
-          <form className="flex flex-col mt-7 space-y-4">
+          <form
+            className="flex flex-col mt-7 space-y-4"
+            onSubmit={handleSubmit}
+          >
             <input
-              type="text"
               autoComplete="off"
               id="name"
               placeholder="نام خود را وارد کنید"
               className="h-11 border border-gray-400/20 focus:border focus:border-green-800/50 px-3 rounded-md placeholder:text-sm text-sm bg-gray-100/50 placeholder:text-gray-500"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
             <input
-              type="text"
               id="email"
               autoComplete="off"
               placeholder="آدرس ایمیل خود را وارد کنید"
               className="h-11 border border-gray-400/20 focus:border focus:border-green-800/50 px-3 rounded-md placeholder:text-sm text-sm bg-gray-100/50 placeholder:text-gray-500"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
             <input
-              type="text"
               autoComplete="off"
               id="pass"
               placeholder="پسوورد خود را وارد کنید"
               className="h-11 border border-gray-400/20 focus:border focus:border-green-800/50 px-3 rounded-md placeholder:text-sm text-sm bg-gray-100/50 placeholder:text-gray-500"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
             />
             <input
-              type="text"
               id="rePass"
               autoComplete="off"
               placeholder="پسوورد را دوباره وارد کنید"
               className="h-11 border border-gray-400/20 focus:border focus:border-green-800/50 px-3 rounded-md placeholder:text-sm text-sm bg-gray-100/50 placeholder:text-gray-500"
+              type="password"
+              value={rePassword}
+              onChange={(e) => setRePassword(e.target.value)}
+              required
             />
             <div className="flex items-center text-sm gap-2">
               <input type="checkbox" />
-
               <span className="text-green-800">
                 قبول تمام قوانین و شرایط استفاده
               </span>
             </div>
-            <button className="bg-green-600 text-white p-3 md:px-8 rounded-md text-sm lg:text-base hover:bg-white hover:transition-colors hover:text-green-800 border-2 border-green-600">
+            <button
+              type="submit"
+              className="bg-green-600 text-white p-3 md:px-8 rounded-md text-sm lg:text-base hover:bg-white hover:transition-colors hover:text-green-800 border-2 border-green-600"
+            >
               ثبت نام
             </button>
+            {message && <p>{message}</p>}
           </form>
         </div>
         <div className="shadow-md border border-gray-400/20 p-5 rounded-md">
@@ -67,7 +119,7 @@ export default function Register() {
         <div className="flex items-center justify-center  gap-2 text-sm  text-green-800">
           <p>GREENY</p>
           <Divider orientation="vertical" flexItem />
-          <div >
+          <div>
             <span>Copyright by</span>
             <i className="icon-copyright"></i>
           </div>
@@ -76,3 +128,5 @@ export default function Register() {
     </div>
   );
 }
+
+export default Register;
