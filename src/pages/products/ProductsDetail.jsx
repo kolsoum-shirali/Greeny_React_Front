0916508d3b218600ea360a155cc5cd53147bbd54
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-17 09:34:14 -0600

    feat : add loading

--- src/pages/products/ProductsDetail.jsx
+++ src/pages/products/ProductsDetail.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import ProductDetailLoading from "../../components/common/ProductDetailLoading";
+import CommentLoading from "../../components/common/CommentLoading";
 
 import ContactForm from "../../components/common/ContactForm";
 import Rating from "../../components/common/Rating";
@@ -53,17 +55,39 @@ export default function ProductsDetail() {
   return (
     <div>
       <BreadCrumbsBanner options={options} caption={product.title} />
-      <div className="container mx-auto p-5 mt-7 md:mt-16 space-y-10 lg:space-y-12">
-        <Product product={product} />
-        <ProductsInfo product={product} />
-        {product.comments && <ProductComments comments={product?.comments} />}
-        <div className="shadow-md rounded-md p-5 md:p-10 border border-gray-400/20">
-          <h3 className="text-lg font-semibold mb-5">نظر خود را اضافه کنید</h3>
-          <div className="flex justify-center mb-5">
-            <Rating />
+      <div className="container mx-auto p-5 mt-7 md:mt-16 ">
+        {loading && !error && (
+          <div class="rounded-md border p-4">
+            <div class="animate-pulse space-y-6">
+              <ProductDetailLoading />
+              <div className="space-y-8">
+                {[...Array(3)].map((_, index) => (
+                  <div key={index}>
+                    <CommentLoading />
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <ContactForm />
-        </div>
+        )}
+        {!loading && !error && product && (
+          <di className="space-y-10 lg:space-y-12">
+            <Product product={product} />
+            <ProductsInfo product={product} />
+            {product.comments && (
+              <ProductComments comments={product?.comments} />
+            )}
+            <div className="shadow-md rounded-md p-5 md:p-10 border border-gray-400/20">
+              <h3 className="text-lg font-semibold mb-5">
+                نظر خود را اضافه کنید
+              </h3>
+              <div className="flex justify-center mb-5">
+                <Rating />
+              </div>
+              <ContactForm />
+            </div>
+          </di>
+        )}
       </div>
     </div>
   );
