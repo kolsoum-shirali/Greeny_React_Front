788759033431c47ec53351a632c168c588c0fa3b
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-17 09:18:18 -0600

    fix : rating

--- src/pages/home/components/comments/SingleComment.jsx
+++ src/pages/home/components/comments/SingleComment.jsx
@@ -1,11 +1,5 @@
 import Rating from "../../../../components/common/Rating";
-import { useState } from "react";
 export default function SingleComment({ comment }) {
-  const [productRate, setProductRate] = useState();
-  const changeProductRate = (newValue) => {
-    setProductRate(newValue);
-  };
-
   return (
     <div className="relative bg-gray-100 px-10 py-5 rounded-md space-y-3 min-h-72">
       <p className="text-justify line-clamp-4 text-gray-700 text-base">
@@ -13,11 +7,7 @@ export default function SingleComment({ comment }) {
       </p>
       <div className="flex flex-col justify-center items-center">
         <h3 className="text-sm">{comment.author}</h3>
-        <Rating
-          rateVal={productRate}
-          isReadable={false}
-          onRatingChange={changeProductRate}
-        />
+        <Rating rateVal={comment.rate} isReadable={true} />
         <div className="h-24  flex justify-center ">
           <img
             src={`${process.env.REACT_APP_BASE_URL_IMG}${comment.img}`}
