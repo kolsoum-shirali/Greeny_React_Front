30f2319a4b5d72ded1b355604552d5cd8cf9cd95
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-19 06:36:15 -0600

    feat: create auth.api.js

---
+++ src/api/auth.api.js
@@ -0,0 +1,23 @@
+import { baseUrl, defaultHeaders } from "./index.api";
+
+export const register = async (userInfo) => {
+  const response = await fetch(`${baseUrl}/auth/register`, {
+    method: "POST",
+    headers: defaultHeaders,
+    body: userInfo,
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    let errorMessage = "An unknown error occurred.";
+    try {
+      const errorData = await response.json();
+      errorMessage =
+        errorData.message || `HTTP error!! status: ${response.status}`;
+    } catch (e) {
+      errorMessage = `HTTP error!!! status: ${response.status}`;
+    }
+    throw new Error(data.message);
+  }
+
+  return data;
+};
