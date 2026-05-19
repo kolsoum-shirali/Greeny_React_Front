0ff16c6b292c4014acbaadd29a6e26cbd504ca2d
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-19 09:43:21 -0600

    add login api in auth.api.js

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -21,3 +21,25 @@ export const register = async (userInfo) => {
 
   return data;
 };
+
+export const login = async (userInfo) => {
+  const response = await fetch(`${baseUrl}/auth/login`, {
+    method: "POST",
+    headers: defaultHeaders,
+    body: userInfo,
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    let errorMessage = "An unknown error occurred.";
+    try {
+      const errorData = await response.json();
+      errorMessage =
+        errorData.message || `HTTP error!! status: ${response.status}`;
+    } catch (e) {
+      errorMessage = `HTTP error!!! status: ${response.status}`;
+    }
+    throw new Error(data.message);
+  }
+
+  return data;
+};
