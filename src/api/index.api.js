4d788ae74986e4ad667b1b2bb9916e24436b4078
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-19 23:17:35 -0600

    update src/api/index.api.js

--- src/api/index.api.js
+++ src/api/index.api.js
@@ -3,6 +3,7 @@ const baseUrl = process.env.REACT_APP_BASE_URL;
 
 const defaultHeaders = {
   "Content-Type": "application/json",
+  
   // You can add other global headers here, e.g., Authorization: `Bearer ${token}`
 };
 
