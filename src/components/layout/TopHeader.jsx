c1dc13e9713db5ab8363743ba525bdb4a8f65274
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-20 01:37:01 -0600

    feat : handle actions of login

--- src/components/layout/TopHeader.jsx
+++ src/components/layout/TopHeader.jsx
@@ -1,10 +1,33 @@
 import Logo from "../../assets/img/logo.png";
 import Divider from "@mui/material/Divider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
+import { useState, useEffect } from "react"; // Import hooks
 
+const actions = [
+  { title: "عضویت", link: "/register" },
+  { title: "ورود", link: "/login" },
+];
 export default function TopHeader({ toggleMobileMenu }) {
-  const {cart} = useCart()
+  const navigate = useNavigate();
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const { cart } = useCart();
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setToken(null);
+    navigate("/");
+    alert("با موفقیت از حساب کاربری خود خارج شدید");
+  };
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem("token"));
+    };
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
   return (
     <div className="flex justify-between items-center p-5 max-lg:fixed top-0 left-0 right-0 z-10 bg-white">
       <i
@@ -13,21 +36,30 @@ export default function TopHeader({ toggleMobileMenu }) {
       ></i>
       <img src={Logo} alt={Logo} className="w-44" />
       <div className="gap-2 hidden lg:flex">
-        <div className="flex items-center gap-2 shadow-md rounded-sm">
-          <Link to="/register" className="p-2">
-            <i className="icon-user-circle-o"></i>
-            عضویت
-          </Link>
-          <Divider orientation="vertical" flexItem />
-          <Link to="/login" className="p-2">
-            <i className="icon-user-circle-o"></i>
-            ورود
-          </Link>
+        <div className="flex items-center gap-2 shadow-md rounded-sm p-3">
+          {!token ? (
+            actions.map((act, inx) => {
+              return (
+                <>
+                  <Link key={inx} to={act.link} className="p-2">
+                    <i className="icon-user-circle-o"></i>
+                    {act.title}
+                  </Link>
+                  {inx === 0 && <Divider orientation="vertical" flexItem />}
+                </>
+              );
+            })
+          ) : (
+            <button onClick={logout}>خروج از حساب کاربری</button>
+          )}
         </div>
-        <Link to="/cart" className="shadow-md rounded-sm p-3 relative">
+        <Link
+          to="/cart"
+          className="shadow-md rounded-sm p-3 relative flex justify-center items-center"
+        >
           <i className="icon-shopping-bag"></i>
-          <span className="bg-green-600 aspect-square rounded-full absolute right-0 -top-2 min-w-6 max-h:h-6 text-center text-white">
-           {cart.length}
+          <span className="bg-green-600 aspect-square rounded-full absolute right-0 -top-2 min-w-7  min-h-7 text-white flex justify-center items-center">
+            {cart.length}
           </span>
         </Link>
       </div>
